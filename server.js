e26e02c6aa0b5ee896e256050a7b221d6dc8741b
whhxd <1757280079@qq.com>
2020-12-03 18:21:54 +0800

    init

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.send('Space Game Server!');
@@ -7,4 +8,8 @@ app.get('/', (req, res) => {
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected.');
 });
