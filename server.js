ba10f7265282e7275ff15129af8c4e7580e73e46
whhxd <1757280079@qq.com>
2020-12-04 14:54:32 +0800

    listen on 80

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, () => {
+http.listen(80, () => {
     console.log('listening on *:3000');
 });
 
